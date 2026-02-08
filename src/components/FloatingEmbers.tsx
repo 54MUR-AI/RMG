262b7f69b75d50347bbae62f8bac19525538ec08
denalifox <denali.2.foxtrot@gmail.com>
2026-02-07 20:23:12 -0500

    CRITICAL: Remove transform from FloatingEmbers container - was breaking viewport locking

--- src/components/FloatingEmbers.tsx
+++ src/components/FloatingEmbers.tsx
@@ -106,7 +106,7 @@ export default function FloatingEmbers() {
   }, [])
 
   return (
-    <div className="fixed inset-0 pointer-events-none" style={{ zIndex: 9999, isolation: 'isolate', transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}>
+    <div className="fixed inset-0 pointer-events-none" style={{ zIndex: 9999, isolation: 'isolate', backfaceVisibility: 'hidden' }}>
       {embers.map((ember) => (
         <div
           key={ember.id}
