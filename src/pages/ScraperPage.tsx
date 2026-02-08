3495eec020b7b87341bdd04fa4317b3b682dcb5e
denalifox <denali.2.foxtrot@gmail.com>
2026-02-08 18:22:18 -0500

    Adjust button positioning for consistent padding from footer
    - Changed bottom-6 to bottom-20 for both SCRP and LDGR buttons
    - Provides balanced spacing from footer matching right-side padding
    - Prevents buttons from being too close to footer

--- src/pages/ScraperPage.tsx
+++ src/pages/ScraperPage.tsx
@@ -78,7 +78,7 @@ export default function ScraperPage() {
       />
 
       {/* Floating Buttons */}
-      <div className="fixed bottom-6 right-6 flex flex-col gap-3 z-50">
+      <div className="fixed bottom-20 right-6 flex flex-col gap-3 z-50">
         {/* README Button */}
         <button
           onClick={() => setShowReadme(true)}
